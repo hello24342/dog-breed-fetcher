b14a60117fe1b262c20bf83e514c7f84cdc1a5ef
hello24342 <hellojennifer.du@mail.utoronto.ca>
2025-10-15 23:05:56 -0400

    task 1

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -19,17 +19,39 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
+     *
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
         // TODO Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        String url = "https://dog.ceo/api/breed/" + breed + "/list";
+        try {
+            Request request = new Request.Builder().url(url).build();
+            try (Response response = client.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    throw new BreedNotFoundException(breed);
+                }
+                String responseBody = response.body().string();
+                JSONObject json = new JSONObject(responseBody);
+                if (json.getString("status").equals("success")) {
+                    JSONArray subbreeds = json.getJSONArray("message");
+                    List<String> result = new ArrayList<>();
+                    for (int i = 0; i < subbreeds.length(); i++) {
+                        result.add(subbreeds.getString(i));
+                    } return result;
+                } else {
+                    throw new BreedNotFoundException(breed);
+                }
+            }
+        } catch (Exception e) {
+            throw new BreedNotFoundException(breed);
+        }
     }
 }
